1ff49be210aa5a9caa43e92f9f4fe125ef2ef886
Dmytro Shevchyk <shevchykdmytro@gmail.com>
2018-07-22 18:19:54 +0300

    KthRowOfPascalsTriangle solution c++

---
+++ c++/Interviewbit/arrays/KthRowOfPascalsTriangle.cpp
@@ -0,0 +1,20 @@
+//
+//  KthRowOfPascalsTriangle.cpp
+//  Interviewbit
+//
+//  Created by Димаська on 22.07.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#include "KthRowOfPascalsTriangle.hpp"
+
+vector<int> Solution::getRow(int A) {
+    vector<int> res(A+1, 0);
+    res[0]=1;
+    for (int i = 0; i <= A ; ++i) {
+        for (int j = i; j > 0; --j) {
+            res[j] = res[j] + res[j-1];
+        }
+    }
+    return res;
+}
---
+++ c++/Interviewbit/arrays/KthRowOfPascalsTriangle.hpp
@@ -0,0 +1,19 @@
+//
+//  KthRowOfPascalsTriangle.hpp
+//  Interviewbit
+//
+//  Created by Димаська on 22.07.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#ifndef KthRowOfPascalsTriangle_hpp
+#define KthRowOfPascalsTriangle_hpp
+
+#include <vector>
+
+using std::vector;
+
+class Solution {
+    vector<int> getRow(int A);
+};
+#endif /* KthRowOfPascalsTriangle_hpp */

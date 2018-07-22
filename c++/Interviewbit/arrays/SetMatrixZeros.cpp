70fdd9b04685ede1d308bc0619a948bc3ccf48ae
Dmytro Shevchyk <shevchykdmytro@gmail.com>
2018-07-22 19:09:58 +0300

    SetMatrixZeros solution c++

---
+++ c++/Interviewbit/arrays/SetMatrixZeros.cpp
@@ -0,0 +1,62 @@
+//
+//  SetMatrixZeros.cpp
+//  Interviewbit
+//
+//  Created by Димаська on 22.07.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#include "SetMatrixZeros.hpp"
+
+void Solution::setZeroes(vector<vector<int>>& A) {
+    if (A.empty() || A.front().empty()) return;
+    auto n = A.size();
+    auto m = A.front().size();
+    bool setZeroFrontRow = false;
+    bool setZeroFrontCol = false;
+    for (unsigned int i = 0; i < n; ++i) {
+        if (A[i][0] == 0) {
+            setZeroFrontCol = true;
+            break;
+        }
+    }
+    for (unsigned int j = 0; j < m; ++j) {
+        if (A[0][j] == 0) {
+            setZeroFrontRow = true;
+            break;
+        }
+    }
+
+    for(int i = 1; i < n; ++i) {
+        for(int j = 1; j < m; ++j) {
+            if(A[i][j] == 0) {
+                A[i][0] = 0;
+                A[0][j] = 0;
+            }
+        }
+    }
+    for (int i = 1; i < n; ++i) {
+        if (A[i][0] == 0) {
+            for (int j = 1; j < m; ++j) {
+                A[i][j] = 0;
+            }
+        }
+    }
+    for (int j = 1; j < m; ++j) {
+        if (A[0][j] == 0) {
+            for (int i = 1; i < n; ++i) {
+                A[i][j] = 0;
+            }
+        }
+    }
+    if (setZeroFrontCol) {
+        for (int i = 0; i < n; ++i) {
+            A[i][0] = 0;
+        }
+    }
+    if (setZeroFrontRow) {
+        for (int j = 0; j < m; ++j) {
+            A[0][j] = 0;
+        }
+    }
+}
---
+++ c++/Interviewbit/arrays/SetMatrixZeros.hpp
@@ -0,0 +1,21 @@
+//
+//  SetMatrixZeros.hpp
+//  Interviewbit
+//
+//  Created by Димаська on 22.07.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#ifndef SetMatrixZeros_hpp
+#define SetMatrixZeros_hpp
+
+#include <vector>
+
+using std::vector;
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix);
+};
+
+#endif /* SetMatrixZeros_hpp */

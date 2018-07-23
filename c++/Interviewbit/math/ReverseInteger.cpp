a7949adc2c196a0ab1679024362f590cd3fa6409
Dmytro Shevchyk <shevchykdmytro@gmail.com>
2018-07-23 21:20:38 +0300

    ReverseInteger solution

---
+++ c++/Interviewbit/math/ReverseInteger.cpp
@@ -0,0 +1,26 @@
+//
+//  ReverseInteger.cpp
+//  Interviewbit
+//
+//  Created by Димаська on 23.07.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#include "ReverseInteger.hpp"
+
+int Solution::reverse(int A) {
+    static constexpr int numericSystem = 10;
+    int res = 0;
+    int sign = A > 0 ? 1 : -1;
+    A = abs(A);
+    int maxVal = numeric_limits<int>::max() / numericSystem;
+    while (A) {
+        if (res > maxVal) return 0;
+        res *= numericSystem;
+        int nextNumb = A % numericSystem;
+        if (res > numeric_limits<int>::max() - nextNumb) return 0;
+        res += nextNumb;
+        A /= numericSystem;
+    }
+    return sign * res;
+}
---
+++ c++/Interviewbit/math/ReverseInteger.hpp
@@ -0,0 +1,15 @@
+//
+//  ReverseInteger.hpp
+//  Interviewbit
+//
+//  Created by Димаська on 23.07.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#ifndef ReverseInteger_hpp
+#define ReverseInteger_hpp
+class Solution {
+    int reverse(int A);
+};
+
+#endif /* ReverseInteger_hpp */

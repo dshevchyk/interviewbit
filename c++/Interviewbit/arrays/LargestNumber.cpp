370e940dbb799d375e82c1b62f22d93e9c72904d
Dmytro Shevchyk <shevchykdmytro@gmail.com>
2018-07-23 21:44:27 +0300

    LargestNumber

---
+++ c++/Interviewbit/arrays/LargestNumber.cpp
@@ -0,0 +1,31 @@
+//
+//  LargestNumber.cpp
+//  Interviewbit
+//
+//  Created by Димаська on 23.07.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#include "LargestNumber.hpp"
+
+using std::to_string;
+
+string Solution::largestNumber(const vector<int> &A) {
+    auto comperator = [](const int &a, const int &b) {
+        auto aStr = to_string(a);
+        auto bStr = to_string(b);
+        auto ab = aStr + bStr;
+        auto ba = bStr + aStr;
+        return ab > ba;
+    };
+    auto ACopy = A;
+    sort(ACopy.begin(), ACopy.end(), comperator);
+    string res;
+    for (auto ai : ACopy) {
+        res += to_string(ai);
+    }
+    if (res[0] == '0') {
+        return "0";
+    }
+    return res;
+}
---
+++ c++/Interviewbit/arrays/LargestNumber.hpp
@@ -0,0 +1,23 @@
+//
+//  LargestNumber.hpp
+//  Interviewbit
+//
+//  Created by Димаська on 23.07.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#ifndef LargestNumber_hpp
+#define LargestNumber_hpp
+
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+class Solution {
+public:
+    string largestNumber(const vector<int> &A);
+};
+
+#endif /* LargestNumber_hpp */

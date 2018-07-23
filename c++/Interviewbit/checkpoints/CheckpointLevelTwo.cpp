8cd7c8d564eecb0e4dafb74a890b9df6b78721d0
Dmytro Shevchyk <shevchykdmytro@gmail.com>
2018-07-23 21:55:31 +0300

    Checkpoint Level 2

---
+++ c++/Interviewbit/checkpoints/CheckpointLevelTwo.cpp
@@ -0,0 +1,26 @@
+//
+//  CheckpointLevelTwo.cpp
+//  Interviewbit
+//
+//  Created by Димаська on 23.07.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#include "CheckpointLevelTwo.hpp"
+vector<vector<int> > Solution::prettyPrint(int A) {
+    int n = A * 2 - 1;
+    vector<vector<int> > res(n, vector<int>(n, 1));
+    for (int i = 0; i < A; ++i) {
+        for (int j = i; j < n - i; ++j) {
+            res[i][j] = A - i;
+            res[n - i - 1][j] = A - i;
+        }
+    }
+    for (int j = 0; j < A; ++j) {
+        for (int i = j + 1; i < n - j - 1; ++i) {
+            res[i][j] = A - j;
+            res[i][n - j -1] = A - j;
+        }
+    }
+    return res;
+}
---
+++ c++/Interviewbit/checkpoints/CheckpointLevelTwo.hpp
@@ -0,0 +1,21 @@
+//
+//  CheckpointLevelTwo.hpp
+//  Interviewbit
+//
+//  Created by Димаська on 23.07.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#ifndef CheckpointLevelTwo_hpp
+#define CheckpointLevelTwo_hpp
+
+#include <vector>
+
+using std::vector;
+
+class Solution {
+public:
+    vector<vector<int> > prettyPrint(int A);
+};
+
+#endif /* CheckpointLevelTwo_hpp */

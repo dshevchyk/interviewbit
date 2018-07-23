0ba0f650d555063998535db6a20a836207d6db5f
Dmytro Shevchyk <shevchykdmytro@gmail.com>
2018-07-23 22:20:47 +0300

    AntiDiagonals solution c++

---
+++ c++/Interviewbit/arrays/AntiDiagonals.cpp
@@ -0,0 +1,37 @@
+//
+//  AntiDiagonals.cpp
+//  Interviewbit
+//
+//  Created by Димаська on 23.07.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#include "AntiDiagonals.hpp"
+
+vector<vector<int> > Solution::diagonal(vector<vector<int> > &A) {
+    int n = A.size();
+    vector<vector<int> > res;
+    for (int j = 0; j < n; ++j) {
+        int i = 0;
+        int k = j;
+        vector<int> diagonal(j + 1, 0);
+        while (k > -1) {
+            diagonal[i]=A[i][k];
+            --k;
+            ++i;
+        }
+        res.push_back(diagonal);
+    }
+    for (int i = 1; i < n; ++i) {
+        int j = n - 1;
+        int k = i;
+        vector<int> diagonal(n - i, 0);
+        while (k < n) {
+            diagonal[k - i]=A[k][j];
+            ++k;
+            --j;
+        }
+        res.push_back(diagonal);
+    }
+    return res;
+}
---
+++ c++/Interviewbit/arrays/AntiDiagonals.hpp
@@ -0,0 +1,20 @@
+//
+//  AntiDiagonals.hpp
+//  Interviewbit
+//
+//  Created by Димаська on 23.07.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#ifndef AntiDiagonals_hpp
+#define AntiDiagonals_hpp
+
+#include <vector>
+
+using std::vector;
+class Solution {
+public:
+    vector<vector<int> > diagonal(vector<vector<int> > &A);
+};
+
+#endif /* AntiDiagonals_hpp */

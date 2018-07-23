667f6879b8fb771e376115fc0178d98a679d4427
Dmytro Shevchyk <shevchykdmytro@gmail.com>
2018-07-23 19:23:07 +0300

    RearrangeArray solution c++

---
+++ c++/Interviewbit/math/RearrangeArray.hpp
@@ -0,0 +1,19 @@
+//
+//  RearrangeArray.hpp
+//  Interviewbit
+//
+//  Created by Димаська on 23.07.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#ifndef RearrangeArray_hpp
+#define RearrangeArray_hpp
+#include <vector>
+
+using std::vector;
+
+class Solution {
+public:
+    void arrange(vector<int> &A);
+};
+#endif /* RearrangeArray_hpp */

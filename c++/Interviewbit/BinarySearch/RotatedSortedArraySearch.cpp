fba913e95dc9e345966d2b532a7cb270e71210b6
Dmytro Shevchyk <shevchykdmytro@gmail.com>
2018-07-29 11:08:13 +0300

    RotatedSortedArraySearch solution c++

---
+++ c++/Interviewbit/BinarySearch/RotatedSortedArraySearch.cpp
@@ -0,0 +1,48 @@
+//
+//  RotatedSortedArraySearch.cpp
+//  Interviewbit
+//
+//  Created by Димаська on 29.07.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#include "RotatedSortedArraySearch.hpp"
+
+vector<int>::const_iterator findMinimun(vector<int>::const_iterator begin,
+                                        vector<int>::const_iterator end) {
+    while (distance(begin, end) > 1) {
+        auto mid = begin + distance(begin, end) / 2;
+        if (*mid >= *(end - 1)) {
+            begin = mid + 1;
+        } else {
+            end = mid + 1;
+        }
+    }
+    return begin;
+}
+
+vector<int>::const_iterator binarySearch(vector<int>::const_iterator begin,
+                                         vector<int>::const_iterator end,
+                                         int target) {
+    auto endCopy = end;
+    while (begin != end) {
+        auto mid = begin + distance(begin, end) / 2;
+        if (*mid == target) {
+            return mid;
+        } else if (*mid > target) {
+            end = mid;
+        } else {
+            begin = mid + 1;
+        }
+    }
+    return endCopy;
+}
+
+int Solution::search(const vector<int> &A, int B) {
+    auto minIt = findMinimun(A.begin(), A.end());
+    auto resIt = binarySearch(A.begin(), minIt, B);
+    if (resIt == minIt) {
+        resIt =  binarySearch(minIt, A.end(), B);
+    }
+    return resIt == A.end() ? -1 : static_cast<int>(distance(A.begin(), resIt));
+}
---
+++ c++/Interviewbit/BinarySearch/RotatedSortedArraySearch.hpp
@@ -0,0 +1,20 @@
+//
+//  RotatedSortedArraySearch.hpp
+//  Interviewbit
+//
+//  Created by Димаська on 29.07.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#ifndef RotatedSortedArraySearch_hpp
+#define RotatedSortedArraySearch_hpp
+
+#include <vector>
+
+using std::vector;
+
+class Solution {
+public:
+    int search(const vector<int> &A, int B);
+};
+#endif /* RotatedSortedArraySearch_hpp */

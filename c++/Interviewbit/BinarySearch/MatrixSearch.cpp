65503f28abc8776c2d53c00a4fb3e1ac3c0dea64
Dmytro Shevchyk <shevchykdmytro@gmail.com>
2018-07-29 11:13:27 +0300

    MatrixSearch solution c++

---
+++ c++/Interviewbit/BinarySearch/MatrixSearch.cpp
@@ -0,0 +1,35 @@
+//
+//  MatrixSearch.cpp
+//  Interviewbit
+//
+//  Created by Димаська on 29.07.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#include "MatrixSearch.hpp"
+
+using std::make_pair;
+using std::pair;
+
+int Solution::searchMatrix(vector<vector<int> > &A, int B) {
+    if (A.empty() || A.front().empty()) return 0;
+    auto n = A.size();
+    auto m = A.front().size();
+    auto indexToMatrix = [m](vector<int>::size_type index) {
+        return make_pair(index / m, index % m);
+    };
+    decltype(m) begin = 0;
+    auto end = n * m;
+    while(begin < end) {
+        auto midIndex = (end - begin) / 2;
+        auto mid = indexToMatrix(midIndex);
+        auto midVal = A[mid.first][mid.second];
+        if (midVal == B) return 1;
+        else if (midVal < B) {
+            begin = midIndex + 1;
+        } else {
+            end = midIndex;
+        }
+    }
+    return 0;
+}
---
+++ c++/Interviewbit/BinarySearch/MatrixSearch.hpp
@@ -0,0 +1,20 @@
+//
+//  MatrixSearch.hpp
+//  Interviewbit
+//
+//  Created by Димаська on 29.07.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#ifndef MatrixSearch_hpp
+#define MatrixSearch_hpp
+
+#include <vector>
+
+using std::vector;
+
+class Solution {
+public:
+    int searchMatrix(vector<vector<int> > &A, int B);
+};
+#endif /* MatrixSearch_hpp */

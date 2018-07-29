0ee06fe5e101435fe6f4be989e16fa12d179ac79
Dmytro Shevchyk <shevchykdmytro@gmail.com>
2018-07-29 11:05:09 +0300

    SquareRootOfInteger solution c++

---
+++ c++/Interviewbit/BinarySearch/SquareRootOfInteger.cpp
@@ -0,0 +1,48 @@
+//
+//  SquareRootOfInteger.cpp
+//  Interviewbit
+//
+//  Created by Димаська on 29.07.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#include "SquareRootOfInteger.hpp"
+
+#include <limits>
+#include <algorithm>
+
+using std::numeric_limits;
+using std::max;
+
+int Solution::sqrt(int A) {
+    // if (A==0 || A==1) return A;
+    // int start = 0, end = A;
+    // int ans;
+    // while(start<=end)
+    // {
+    //     int mid = start + (end - start)/2;
+    //     if (mid <= A/mid)
+    //     {
+    //         start = mid + 1;
+    //         ans = mid;
+    //     }
+    //     else
+    //         end = mid - 1;
+    // }
+    // return ans;
+    int start = 0;
+    int end = A+1;
+    int best = numeric_limits<int>::min();
+    while (start < end) {
+        int mid = start + (end - start) / 2;
+        int candidate = mid * mid;
+        if(candidate == A) return mid;
+        if (candidate < A ) {
+            best = max(best, candidate);
+            start = mid+1;
+        } else {
+            end = mid;
+        }
+    }
+    return best;
+}
---
+++ c++/Interviewbit/BinarySearch/SquareRootOfInteger.hpp
@@ -0,0 +1,16 @@
+//
+//  SquareRootOfInteger.hpp
+//  Interviewbit
+//
+//  Created by Димаська on 29.07.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#ifndef SquareRootOfInteger_hpp
+#define SquareRootOfInteger_hpp
+
+class Solution {
+    int sqrt(int A);
+};
+
+#endif /* SquareRootOfInteger_hpp */

525bad60f202a50345bb007f51ad3908b2e84c3d
Dmytro Shevchyk <shevchykdmytro@gmail.com>
2018-08-04 13:13:55 +0300

    LongestCommonPrefix soluition c++

---
+++ c++/Interviewbit/Strings/LongestCommonPrefix.cpp
@@ -0,0 +1,22 @@
+//
+//  LongestCommonPrefix.cpp
+//  Interviewbit
+//
+//  Created by Димаська on 04.08.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#include "LongestCommonPrefix.hpp"
+
+string Solution::longestCommonPrefix(vector<string> &A) {
+    if (A.empty()) return string();
+    const auto maxlength = A.front().size();
+    for (int i = 0; i < maxlength; ++i) {
+        for (int j = 1; j < A.size(); ++j) {
+            if (A[j].size() < i + 1 || A[j][i] != A.front()[i]) {
+                return string(A.front().begin(), A.front().begin() + i);
+            }
+        }
+    }
+    return A.front();
+}
---
+++ c++/Interviewbit/Strings/LongestCommonPrefix.hpp
@@ -0,0 +1,23 @@
+//
+//  LongestCommonPrefix.hpp
+//  Interviewbit
+//
+//  Created by Димаська on 04.08.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#ifndef LongestCommonPrefix_hpp
+#define LongestCommonPrefix_hpp
+
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string> &A);
+};
+
+#endif /* LongestCommonPrefix_hpp */

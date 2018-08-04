01849fc2120eec6f17a3a6e76e5d849dade86db0
Dmytro Shevchyk <shevchykdmytro@gmail.com>
2018-08-04 14:54:05 +0300

    atoi solution c++

---
+++ c++/Interviewbit/Strings/Atoi.cpp
@@ -0,0 +1,37 @@
+//
+//  Atoi.cpp
+//  Interviewbit
+//
+//  Created by Димаська on 04.08.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#include "Atoi.hpp"
+#include <limits>
+
+using std::numeric_limits;
+
+bool isValid(char c) {
+    return c >= '0' && c <= '9';
+}
+
+int Solution::atoi(const string A) {
+    auto current = A.begin();
+    while (current != A.end() && *current == ' ') ++ current;
+    if (current == A.end()) return 0;
+    int sign = 1;
+    if (*current == '+' || *current == '-') {
+        sign = *current == '-' ? -1 : 1;
+        ++current;
+    }
+    int res = 0;
+    int overflowValue = numeric_limits<int>::max() / 10;
+    while(current != A.end() && isValid(*current)) {
+        if (res >= overflowValue) {
+            return sign == 1 ? numeric_limits<int>::max() : numeric_limits<int>::min();
+        }
+        res *= 10;
+        res += *(current++) - '0';
+    }
+    return res * sign;
+}
---
+++ c++/Interviewbit/Strings/Atoi.hpp
@@ -0,0 +1,21 @@
+//
+//  Atoi.hpp
+//  Interviewbit
+//
+//  Created by Димаська on 04.08.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#ifndef Atoi_hpp
+#define Atoi_hpp
+
+#include <string>
+
+using std::string;
+
+class Solution {
+public:
+    int atoi(const string A);
+};
+
+#endif /* Atoi_hpp */

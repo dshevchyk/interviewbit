e616b6a437fa51764605e5294951df61e3f533cf
Dmytro Shevchyk <shevchykdmytro@gmail.com>
2018-08-04 14:38:39 +0300

    LongestPalindromicSubstring solution c++

---
+++ c++/Interviewbit/Strings/LongestPalindromicSubstring.cpp
@@ -0,0 +1,40 @@
+//
+//  LongestPalindromicSubstring.cpp
+//  Interviewbit
+//
+//  Created by Димаська on 04.08.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#include "LongestPalindromicSubstring.hpp"
+
+#include <algorithm>
+
+using std::max;
+
+namespace {
+    int palindromSize(const string& A, int left, int right) {
+        while (left >= 0 && right < A.size() && A[left] == A[right]) {
+            ++right;
+            --left;
+        }
+        return right - left - 1;
+    }
+}
+
+string Solution::longestPalindrome(string A) {
+    int begin  = 0;
+    int end = 0;
+    int maxlen = 0;
+    for (int  i = 0; i < A.size(); ++i) {
+        int len1 = palindromSize(A, i, i);
+        int len2 = palindromSize(A, i, i + 1);
+        int len = max(len1, len2);
+        if (len > maxlen) {
+            begin = i - (len -1) / 2;
+            end = i + len / 2;
+            maxlen = len;
+        }
+    }
+    return string(A.begin()+ begin, A.begin() + end+1);
+}
---
+++ c++/Interviewbit/Strings/LongestPalindromicSubstring.hpp
@@ -0,0 +1,21 @@
+//
+//  LongestPalindromicSubstring.hpp
+//  Interviewbit
+//
+//  Created by Димаська on 04.08.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#ifndef LongestPalindromicSubstring_hpp
+#define LongestPalindromicSubstring_hpp
+
+#include <string>
+
+using std::string;
+
+class Solution {
+public:
+    string longestPalindrome(string A);
+};
+
+#endif /* LongestPalindromicSubstring_hpp */

b268a12a721f9c0ff273ccfee29ce472c7e9b131
Dmytro Shevchyk <shevchykdmytro@gmail.com>
2018-08-05 14:45:10 +0300

    ZigzagString solution c++

---
+++ c++/Interviewbit/Strings/ZigzagString.cpp
@@ -0,0 +1,33 @@
+//
+//  ZigzagString.cpp
+//  Interviewbit
+//
+//  Created by Димаська on 05.08.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#include "ZigzagString.hpp"
+
+#include <vector>
+
+using std::vector;
+
+string Solution::convert(string A, int B) {
+    if (B < 2) return A;
+    vector<string> lines(B, string());
+    int currentRow = 0;
+    int direction = 1;
+    for (int i = 0; i < A.size(); ++i) {
+        lines[currentRow].push_back(A[i]);
+        currentRow += direction;
+        if(currentRow < 0 || currentRow >= B) {
+            direction *= -1;
+            currentRow += 2 * direction;
+        }
+    }
+    string res;
+    for (const auto& line: lines) {
+        res += line;
+    }
+    return res;
+}
---
+++ c++/Interviewbit/Strings/ZigzagString.hpp
@@ -0,0 +1,21 @@
+//
+//  ZigzagString.hpp
+//  Interviewbit
+//
+//  Created by Димаська on 05.08.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#ifndef ZigzagString_hpp
+#define ZigzagString_hpp
+
+#include <string>
+
+using std::string;
+
+class Solution {
+public:
+    string convert(string A, int B);
+};
+
+#endif /* ZigzagString_hpp */

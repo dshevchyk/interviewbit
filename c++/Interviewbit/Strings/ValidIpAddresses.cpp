9f8d610dd83f1e1de2ade55478da2d7d6350bc05
Dmytro Shevchyk <shevchykdmytro@gmail.com>
2018-08-05 15:50:32 +0300

    ValidIpAddresses solution c++

---
+++ c++/Interviewbit/Strings/ValidIpAddresses.cpp
@@ -0,0 +1,48 @@
+//
+//  ValidIpAddresses.cpp
+//  Interviewbit
+//
+//  Created by Димаська on 05.08.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#include "ValidIpAddresses.hpp"
+
+namespace {
+    
+static constexpr int maxNumbLength = 3;
+static constexpr int maxNumbValue = 255;
+static constexpr int ipSize = 4;
+    
+void dfs (const string& A,
+          int start,
+          const int stepsLeft,
+          const string& ip,
+          vector<string>& result) {
+    const auto n = A.size();
+    if (start == n && stepsLeft == 0){
+        result.push_back(ip);
+        return;
+    }
+    int num = 0;
+    string nextIp = ip.empty() ? ip : ip + ".";
+    for (int i = start; i < start + maxNumbLength && i < n; i++) {
+        num = num * 10 + (A[i] - '0');
+        if (num <= maxNumbValue) {
+            nextIp += A[i];
+            dfs(A, i + 1, stepsLeft - 1, nextIp, result);
+        }
+        if (num >= maxNumbValue || num < 1) {
+            break;
+        }
+    }
+}
+
+}
+
+vector<string> Solution::restoreIpAddresses(string A) {
+    vector<string> results;
+    string ip;
+    dfs(A, 0, ipSize, ip, results);
+    return results;
+}
---
+++ c++/Interviewbit/Strings/ValidIpAddresses.hpp
@@ -0,0 +1,23 @@
+//
+//  ValidIpAddresses.hpp
+//  Interviewbit
+//
+//  Created by Димаська on 05.08.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#ifndef ValidIpAddresses_hpp
+#define ValidIpAddresses_hpp
+
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+class Solution {
+public:
+    vector<string> restoreIpAddresses(string A);
+};
+
+#endif /* ValidIpAddresses_hpp */

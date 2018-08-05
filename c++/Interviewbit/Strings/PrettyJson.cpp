6e84ba7dcd131988b9b3c01f046c3226dbe8d2ac
Dmytro Shevchyk <shevchykdmytro@gmail.com>
2018-08-05 17:08:23 +0300

    PrettyJson solution c++

---
+++ c++/Interviewbit/Strings/PrettyJson.cpp
@@ -0,0 +1,62 @@
+//
+//  PrettyJson.cpp
+//  Interviewbit
+//
+//  Created by Димаська on 05.08.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#include "PrettyJson.hpp"
+
+#include <algorithm>
+
+using std::max;
+
+vector<string> Solution::prettyJSON(string A) {
+    vector<string> result;
+    const char space = ' ';
+    const auto n = A.size();
+    int bracesCount = 0;
+    string currentLine;
+    A.erase(remove(A.begin(), A.end(), space), A.end());
+    auto insertLineCallback = [&result, &currentLine, &bracesCount] (int bracesIncrement) {
+        result.push_back(currentLine);
+        bracesCount = max(0, bracesCount + bracesIncrement);
+        currentLine = string(bracesCount, '\t');
+    };
+    for (int i = 0; i < n; ++i) {
+        currentLine.push_back(A[i]);
+        int nextI = i + 1;
+        switch (A[i]) {
+            case '{':
+            case '[': {
+                insertLineCallback(1);
+            }
+            break;
+            case '}':
+            case ']': {
+                if (nextI == n || A[nextI] != ',') {
+                    insertLineCallback(-1);
+                }
+            }
+            break;
+            case ',': {
+                insertLineCallback(0);
+            }
+            break;
+            case ':': {
+                if (A[nextI] == '{' || A[nextI] == '[') {
+                    insertLineCallback(0);
+                }
+            }
+            break;
+            default : {
+                if (nextI < n && (A[nextI] == '}' || A[nextI] == ']')) {
+                    insertLineCallback(-1);
+                }
+            }
+        }
+    }
+    return result;
+}
+
---
+++ c++/Interviewbit/Strings/PrettyJson.hpp
@@ -0,0 +1,23 @@
+//
+//  PrettyJson.hpp
+//  Interviewbit
+//
+//  Created by Димаська on 05.08.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#ifndef PrettyJson_hpp
+#define PrettyJson_hpp
+
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+class Solution {
+public:
+    vector<string> prettyJSON(string A);
+};
+
+#endif /* PrettyJson_hpp */

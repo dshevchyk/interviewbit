d78f6433c1dfd82f1f6e4f5aa8d4181ba150855a
Dmytro Shevchyk <shevchykdmytro@gmail.com>
2018-08-07 22:09:08 +0300

    SortByColor solution c++

---
+++ c++/Interviewbit/TwoPointers/SortByColor.cpp
@@ -0,0 +1,23 @@
+//
+//  SortByColor.cpp
+//  Interviewbit
+//
+//  Created by Димаська on 07.08.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#include "SortByColor.hpp"
+#include <array>
+
+using std::array;
+
+void Solution::sortColors(vector<int> &A) {
+    array<int, 3> counter{0, 0, 0};
+    for (auto it = A.begin(); it != A.end(); ++it) {
+        ++counter[*it];
+    }
+    for (int i = 0, shift = 0; i < counter.size(); ++i) {
+        fill_n(A.begin() + shift, counter[i], i);
+        shift += counter[i];
+    }
+}
---
+++ c++/Interviewbit/TwoPointers/SortByColor.hpp
@@ -0,0 +1,20 @@
+//
+//  SortByColor.hpp
+//  Interviewbit
+//
+//  Created by Димаська on 07.08.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#ifndef SortByColor_hpp
+#define SortByColor_hpp
+
+#include <vector>
+
+using std::vector;
+
+class Solution {
+public:
+    void sortColors(vector<int> &A);
+};
+#endif /* SortByColor_hpp */

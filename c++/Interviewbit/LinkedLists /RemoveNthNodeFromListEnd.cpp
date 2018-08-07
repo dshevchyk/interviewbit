c18de8fa2cccc104df3bef8f31192a3f0f2e2a55
Dmytro Shevchyk <shevchykdmytro@gmail.com>
2018-08-07 20:11:08 +0300

    RemoveNthNodeFromListEnd solution c++

---
+++ c++/Interviewbit/LinkedLists /RemoveNthNodeFromListEnd.cpp
@@ -0,0 +1,9 @@
+//
+//  RemoveNthNodeFromListEnd.cpp
+//  Interviewbit
+//
+//  Created by Димаська on 07.08.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#include "RemoveNthNodeFromListEnd.hpp"
---
+++ c++/Interviewbit/LinkedLists /RemoveNthNodeFromListEnd.hpp
@@ -0,0 +1,14 @@
+//
+//  RemoveNthNodeFromListEnd.hpp
+//  Interviewbit
+//
+//  Created by Димаська on 07.08.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#ifndef RemoveNthNodeFromListEnd_hpp
+#define RemoveNthNodeFromListEnd_hpp
+
+#include <stdio.h>
+
+#endif /* RemoveNthNodeFromListEnd_hpp */

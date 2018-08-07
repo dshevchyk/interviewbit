e7a5ab3c851d84d61464088c128e78515b70dba4
Dmytro Shevchyk <shevchykdmytro@gmail.com>
2018-08-07 22:11:59 +0300

    MaxContinuousSeriesOf1s solution c++

---
+++ c++/Interviewbit/TwoPointers/MaxContinuousSeriesOf1s.cpp
@@ -0,0 +1,47 @@
+//
+//  MaxContinuousSeriesOf1s.cpp
+//  Interviewbit
+//
+//  Created by Димаська on 07.08.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#include "MaxContinuousSeriesOf1s.hpp"
+#include <algorithm>
+
+using std::max;
+
+vector<int> Solution::maxone(vector<int> &A, int B) {
+    int start = 0;
+    int end = 0;
+    int beststart = 0;
+    int bestend = 0;
+    int swaps = 0;
+    
+    while (end < A.size()) {
+        if (A[end] == 1) {
+            ++end;
+        } else if (swaps < B) {
+            ++swaps;
+            ++end;
+        } else {
+            if(bestend - beststart < end - start) {
+                beststart = start;
+                bestend = end;
+            }
+            if (A[start++] == 0) {
+                --swaps;
+            }
+            swaps = max(swaps, 0);
+            end = max(start, end);
+        }
+    }
+    if(bestend - beststart < end - start) {
+        beststart = start;
+        bestend = end;
+    }
+    vector<int> res(bestend - beststart);
+    int n=beststart;
+    generate(res.begin(), res.end(), [&n] { return n++;});
+    return res;
+}
---
+++ c++/Interviewbit/TwoPointers/MaxContinuousSeriesOf1s.hpp
@@ -0,0 +1,21 @@
+//
+//  MaxContinuousSeriesOf1s.hpp
+//  Interviewbit
+//
+//  Created by Димаська on 07.08.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#ifndef MaxContinuousSeriesOf1s_hpp
+#define MaxContinuousSeriesOf1s_hpp
+
+#include <vector>
+
+using std::vector;
+
+class Solution {
+public:
+    vector<int> maxone(vector<int> &A, int B);
+};
+
+#endif /* MaxContinuousSeriesOf1s_hpp */

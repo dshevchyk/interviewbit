3bbd9a4921c80d293cefda8cded5e10325a050a0
Dmytro Shevchyk <shevchykdmytro@gmail.com>
2018-08-07 22:14:51 +0300

    ContainerWithMostWater solution c++

---
+++ c++/Interviewbit/TwoPointers/ContainerWithMostWater.cpp
@@ -0,0 +1,32 @@
+//
+//  ContainerWithMostWater.cpp
+//  Interviewbit
+//
+//  Created by Димаська on 07.08.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#include "ContainerWithMostWater.hpp"
+#include <algorithm>
+
+using std::min;
+
+int Solution::maxArea(vector<int> &A) {
+    int i = 0;
+    auto j = A.size() - 1;
+    int bestAnswer = 0;
+    while(i < j) {
+        auto width = j - i;
+        auto height = min(A[i], A[j]);
+        auto candidate = width * height;
+        if (bestAnswer < candidate) {
+            bestAnswer = static_cast<int>(candidate);
+        }
+        if (A[i] < A[j]) {
+            ++i;
+        } else {
+            --j;
+        }
+    }
+    return bestAnswer;
+}
---
+++ c++/Interviewbit/TwoPointers/ContainerWithMostWater.hpp
@@ -0,0 +1,21 @@
+//
+//  ContainerWithMostWater.hpp
+//  Interviewbit
+//
+//  Created by Димаська on 07.08.2018.
+//  Copyright © 2018 Димаська. All rights reserved.
+//
+
+#ifndef ContainerWithMostWater_hpp
+#define ContainerWithMostWater_hpp
+
+#include <vector>
+
+using std::vector;
+
+class Solution {
+public:
+    int maxArea(vector<int> &A);
+};
+
+#endif /* ContainerWithMostWater_hpp */

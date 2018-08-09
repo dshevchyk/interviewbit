19e28ead87ce3831fd47024cc967bc883eddc7e8
Dmytro Shevchyk <shevchykdmytro@gmail.com>
2018-08-09 22:29:18 +0300

    DiffkIi solution c++

---
+++ c++/Interviewbit/Hashing/DiffkIi.cpp
@@ -0,0 +1,10 @@
+////
+//  DiffkIi.cpp
+//  Interviewbit
+//
+//  Created by Shevchyk Dmytro on 09.08.2018.
+//  Copyright © 2018 Shevchyk Dmytro. All rights reserved.
+//
+
+
+#include "DiffkIi.hpp"
---
+++ c++/Interviewbit/Hashing/DiffkIi.hpp
@@ -0,0 +1,15 @@
+////
+//  DiffkIi.hpp
+//  Interviewbit
+//
+//  Created by Shevchyk Dmytro on 09.08.2018.
+//  Copyright © 2018 Shevchyk Dmytro. All rights reserved.
+//
+
+
+#ifndef DiffkIi_hpp
+#define DiffkIi_hpp
+
+#include <stdio.h>
+
+#endif /* DiffkIi_hpp */
